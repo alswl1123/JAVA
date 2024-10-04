3094e162ff109990a1f42910405c7751fd3c5556
김민지 <@LAPTOP-MU0TM0GH>
2024-10-04 19:26:39 +0900

    sleep 예제(1초씩 결과값 출력)

--- java_connected_with_git/src/java_connected_with_git/sleep_example.java
+++ java_connected_with_git/src/java_connected_with_git/sleep_example.java
@@ -2,16 +2,13 @@ package java_connected_with_git;
 
 public class sleep_example {
 
-	public static void main(String[] args) 
-	{
-		for(int i = 10; i >= 0; i--) //10부터 0까지 감소
-		//for(int i = 0; i <= 10; i++) //이렇게도 만들 수 있다. 0부터 10까지 증가
+	public static void main(String[] args) {
+		for (int i = 10; i >= 0; i--) // 10부터 0까지 감소
+		// for(int i = 0; i <= 10; i++) //이렇게도 만들 수 있다. 0부터 10까지 증가
 		{
-			try 
-			{
+			try {
 				Thread.sleep(1000);
-			}
-			catch(Exception e) //catch에 매개변수 주는 것 잊으면 안 된다.
+			} catch (Exception e) // catch에 매개변수 주는 것 잊으면 안 된다.
 			{
 				e.printStackTrace();
 			}
