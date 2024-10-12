8967821a743500a0e6d576d1fc8946e52915906a
Minjee <grace_123@nate.com>
2024-10-12 22:38:06 +0900

    내가 조금 손 본 주사위 예제 (n 를 선택했을 때의 분기점도 만들어보았다)

--- java_connected_with_git/src/java_connected_with_git/dice_random.java
+++ java_connected_with_git/src/java_connected_with_git/dice_random.java
@@ -10,27 +10,35 @@ public class dice_random {
 		
 		String answer = "y"; //값 초기화 안 해도 nextLine에서 입력 받으므로 괜찮다.
 		
-		Scanner input = new Scanner(System.in);
+		
 		
 		while(answer.equals("y")) {
+
+			System.out.println("주사위를 굴리겠습니까? y or n");
+			Scanner input = new Scanner(System.in);
+			answer = input.nextLine();
+			if(answer.equals("n")) {
+				break;
+			}
+
 			
 			//1~6 사이의 값을 랜덤하게 추출(+1 없이 6만 넣으면 0~5 반환)
-			
 			int first = rd.nextInt(6) + 1; //항상 함수가 먼저 실행됨
 			int second = rd.nextInt(6) + 1;
 			
-			System.out.println("주사위를 굴리겠습니까? y or n");
-			answer = input.nextLine();
+				System.out.println("첫 번째 주사위 값 : " + first);
+				System.out.println("두 번째 주사위 값 : " + second);
 			
-			System.out.println("첫 번째 주사위 값 : " + first);
-			System.out.println("두 번째 주사위 값 : " + second);
-			
-			if (first == second) {
-				System.out.println("축하합니다. 더블입니다!");
-			}
-			
-		}
+				
+				if(first == second) 
+				{
+					System.out.println("축하합니다. 더블입니다!");
+				}
+				continue;
 		
+			}System.out.println("게임이 종료되었습니다.");
+		}
 	}
 
-}
+	
+
