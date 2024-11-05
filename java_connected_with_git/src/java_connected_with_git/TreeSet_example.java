9de2506c9f93cddfb1fba9f56bdf259b250e1be2
Minjee <grace_123@nate.com>
2024-11-05 23:22:07 +0900

    treeSet 예제가 기억이 안 나서 연습. set 인터페이스 구현. Hs 는 중복x 정렬x Ts 는 중복x 정렬o

---
+++ java_connected_with_git/src/java_connected_with_git/TreeSet_example.java
@@ -0,0 +1,45 @@
+package java_connected_with_git;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class TreeSet_example {
+	
+	public static void main(String[] args) {
+		HashSet hs = new HashSet();
+		
+		hs.add("demon");
+		hs.add("banana");
+		hs.add("tomato");
+		hs.add("apple");
+		hs.add("cargo");
+		
+		TreeSet ts = new TreeSet();
+		ts.add("demon");
+		ts.add("banana");
+		ts.add("tomato");
+		ts.add("apple");
+		ts.add("cargo");
+		
+		Iterator it = hs.iterator();
+		System.out.println("<HashSet 출력>");
+		
+		while(it.hasNext())
+		{
+			System.out.println(" " + it.next());
+		}
+		
+		Iterator it2 = ts.iterator();
+		System.out.println("\n<TreeSet 출력>");
+		
+		while(it2.hasNext())
+		{
+			System.out.println(" " + it2.next());
+		}
+		
+		System.out.println("\n현재 TreeSet의 크기 : " + ts.size());
+		
+	}
+
+}
